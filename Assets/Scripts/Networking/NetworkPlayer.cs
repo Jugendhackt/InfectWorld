6b719bf5400c1adb42c1886c3cc12c71c3c258ed
CodeDoctorDE <wusel905@gmail.com>
2020-11-28 20:23:23 +0100

    fixing camera

--- Assets/Scripts/Networking/NetworkPlayer.cs
+++ Assets/Scripts/Networking/NetworkPlayer.cs
@@ -3,13 +3,14 @@ using Photon.Pun;
 using UnityEngine;
 
 public class NetworkPlayer : MonoBehaviour
-    {
+{
+    public GameObject camera;
         private void Start()
         {
             if (GetComponent<PhotonView>().IsMine)
             {
                 GetComponent<PlayerMovement>().enabled = true;
-                GetComponentInChildren<Camera>().gameObject.SetActive(true);
+                camera.SetActive(true);
             }
         }
     }

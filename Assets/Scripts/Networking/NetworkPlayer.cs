44e771f89e4545fcaf665b76af16685d4416e5d6
CodeDoctorDE <wusel905@gmail.com>
2020-11-28 23:25:22 +0100

    add patient progress

--- Assets/Scripts/Networking/NetworkPlayer.cs
+++ Assets/Scripts/Networking/NetworkPlayer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using Bolt;
 using Photon.Pun;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -7,6 +9,8 @@ public class NetworkPlayer : MonoBehaviourPun
 {
     public GameObject camera;
     private PlayerState _playerState;
+    public int infectRange = 3;
+    public int infectTime = 10;
     private void Start()
     {
         _playerState = PlayerState.life;
@@ -20,6 +24,42 @@ public class NetworkPlayer : MonoBehaviourPun
         if (!PhotonNetwork.IsMasterClient || !photonView.IsMine) return;
         var randomPlayer = Random.Range(0, PhotonNetwork.PlayerList.Length);
         photonView.RPC("RPC_SelectPatient", PhotonNetwork.CurrentRoom.GetPlayer(randomPlayer));
+    }
+
+    private GameObject _currentNearestPlayer;
+    private float _currentTimeInfected = 0;
+
+    private void FixedUpdate()
+    {
+        if (_currentNearestPlayer == null || Vector3.Distance(transform.position, _currentNearestPlayer.transform.position) > infectRange)
+        {
+            GameObject bestTarget = null;
+            var closestDistanceSqr = Mathf.Infinity;
+            var currentPosition = transform.position;
+            // Keine Patienten!
+            foreach(var potentialTarget in GameObject.FindGameObjectsWithTag("player"))
+            {
+                var directionToTarget = potentialTarget.transform.position - currentPosition;
+                var dSqrToTarget = directionToTarget.sqrMagnitude;
+                if (!(dSqrToTarget < closestDistanceSqr)) continue;
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+
+            _currentNearestPlayer = bestTarget;
+            _currentTimeInfected = 0;
+        }
+        else
+            _currentTimeInfected += Time.fixedDeltaTime;
+
+        var percent = _currentTimeInfected / infectTime;
+        FindObjectOfType<LevelUIScript>().ChangeInfectedTime(percent);
+        if (percent > 1f)
+        {
+            // Infiziere andere!
+            _currentTimeInfected = 0;
+            _currentNearestPlayer = null;
+        }
     }
 
     [PunRPC]
--- Assets/Scripts/UI/LevelUIScript.cs
+++ Assets/Scripts/UI/LevelUIScript.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUIScript : MonoBehaviour
 {
         public GameObject patientView;
+        public Image infectedProgress;
+        public TMP_Text infectedProgressText;
         
         public void ChangePatientView()
         { 
                 patientView.SetActive(true);
+        }
+
+        public void ChangeInfectedTime(float timePercent)
+        {
+                infectedProgress.fillAmount = timePercent;
+                infectedProgressText.text = $"{Math.Round(timePercent * 100)}% infected";
         }
 }
